e8a320f0f93626c24f2684b6e4ffb62ac29e6951
Andrew Leech <andrew@alelec.net>
2014-07-22 20:03:46 +1000

    Migrate from using sockets to tinyrpc for communication. Automatically send current status on change or once per 5 minutes

--- rpi_tv_power_server.py
+++ rpi_tv_power_server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import urllib2
 from subprocess import PIPE, Popen, call, check_output, CalledProcessError
-import xmltodict
+#import xmltodict
 import time
 from threading import Thread
 import os
@@ -16,6 +16,14 @@ import Queue
 
 import mythtv_status
 import xbmc_status
+
+import zmq
+
+from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
+from tinyrpc.transports.zmq import ZmqClientTransport
+from tinyrpc import RPCClient
+
+# import Pyro4
 
 #pydevd.settrace('192.168.0.9', port=2345, stdoutToServer=True, stderrToServer=True)
 
@@ -45,9 +53,10 @@ def debugThread(my_globals):
 #dt.daemon = True # thread dies with the program
 #dt.start()
 
-
+SERVIER_ADDR = "tcp://rpi.local:6601"
 
 TIMEOUT = 10 * 60 #seconds
+RESEND_TIMEOUT = 5 * 60 #seconds
 
 
 ###########
@@ -68,7 +77,6 @@ resetQueue = Queue.Queue()
 #############################
 # Regular Keypress Monitoring
 
-keyPressTimeout = time.time() - TIMEOUT + 90    # Start with 1.5 minute to sleep
 
 def OnKeyDownEvent(ignored):
     #print "press"
@@ -167,91 +175,122 @@ def network_server(s):
             logger.warn("exception in network_server(): " + str(e))
 
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 55555                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+def main():
 
-s.listen(5)                 # Now wait for client connection.
-
-nt = Thread(target=network_server, args=(s,))
-nt.daemon = True # thread dies with the program
-nt.start()
-
-
-#############################
-# Runtime Loop
-
-global sleepActive
-sleepActive = False
-
-global timeout
-global lastNetwork
-timeout = 0
-lastNetwork = time.time()
-playstate = 'idle'
-prevplaystate = playstate
-
-lastStat = time.time()
-last_mythtv_status = False
-last_xbmc_status = False
-
-mythtvStatus = mythtv_status.mythtv_status()
-
-while True:
     try:
 
-        if not resetQueue.empty():
-            resetQueue.get(timeout=10)
-            keyPressTimeout = time.time()
+        # s = socket.socket()         # Create a socket object
+        # host = socket.gethostname() # Get local machine name
+        # port = 55555                # Reserve a port for your service.
+        # s.bind((host, port))        # Bind to the port
+        #
+        # s.listen(5)                 # Now wait for client connection.
+        #
+        # nt = Thread(target=network_server, args=(s,))
+        # nt.daemon = True # thread dies with the program
+        # nt.start()
 
-        if (time.time() - lastStat) > 60:
-            logger.debug("idleTime: " + str((time.time() - keyPressTimeout)/60))
-            lastStat = time.time()
+        ctx = zmq.Context()
 
-        # Monitor MythTV
-        current_mythtv_status = mythtvStatus.getStatus()
-        if current_mythtv_status:
-            if not last_mythtv_status:
-                logger.debug("mythtv start playing")
-            keyPressTimeout = time.time()
-        elif last_mythtv_status:
-            logger.debug("mythtv stop playing")
-        last_mythtv_status = current_mythtv_status
+        rpc_client = RPCClient(
+            JSONRPCProtocol(),
+            ZmqClientTransport.create(ctx, SERVIER_ADDR)
+        )
 
-        # Monitor XBMC
-        try:
-            current_xbmc_status = xbmc_status.getStatus()
-            if current_xbmc_status:
-                if not last_xbmc_status:
-                    logger.debug("xbmc start playing")
+        remote_server = rpc_client.get_proxy()
+        tv_controller = rpc_client.get_proxy("tv_controller.")
+
+        #############################
+        # Runtime Loop
+
+        global sleepActive
+        sleepActive = False
+
+        global timeout
+        global lastNetwork
+        timeout = 0
+        lastNetwork = time.time()
+        playstate = 'idle'
+        prevplaystate = playstate
+
+        lastStat = time.time()
+        last_mythtv_status = False
+        last_xbmc_status = False
+
+        mythtvStatus = mythtv_status.mythtv_status()
+
+        keyPressTimeout = time.time() - TIMEOUT + 90    # Start with 1.5 minute to sleep
+
+        timeSinceComms = time.time() - RESEND_TIMEOUT + 20
+
+        while True:
+            if not resetQueue.empty():
+                resetQueue.get(timeout=10)
                 keyPressTimeout = time.time()
-            elif last_xbmc_status:
-                logger.debug("xbmc stop playing")
-            last_xbmc_status = current_xbmc_status
-        except xbmc_status.Timeout:
-            pass
 
-        if time.time() - keyPressTimeout > TIMEOUT:
-            # Go To Sleep
-            if not sleepActive:
-                print "Sleep"
-                logger.debug("timeout: sleep")
-            sleepActive = True
+            if (time.time() - lastStat) > 60:
+                logger.debug("idleTime: " + str((time.time() - keyPressTimeout)/60))
+                lastStat = time.time()
 
-            # Comms has dropped out, let system service manager restart
-            if time.time() - lastNetwork > TIMEOUT:
-                exit()
+            # Monitor MythTV
+            current_mythtv_status = mythtvStatus.getStatus()
+            if current_mythtv_status:
+                if not last_mythtv_status:
+                    logger.debug("mythtv start playing")
+                keyPressTimeout = time.time()
+            elif last_mythtv_status:
+                logger.debug("mythtv stop playing")
+            last_mythtv_status = current_mythtv_status
 
-        else:
-            if sleepActive:
-                print "Wake"
-                logger.debug("timeout: wake")
-            sleepActive = False
+            # Monitor XBMC
+            try:
+                current_xbmc_status = xbmc_status.getStatus()
+                if current_xbmc_status:
+                    if not last_xbmc_status:
+                        logger.debug("xbmc start playing")
+                    keyPressTimeout = time.time()
+                elif last_xbmc_status:
+                    logger.debug("xbmc stop playing")
+                last_xbmc_status = current_xbmc_status
+            except xbmc_status.Timeout:
+                pass
 
-        time.sleep(0.5)
+            now = time.time()
+            sleepActive = (now - keyPressTimeout) > TIMEOUT
+
+            if now - timeSinceComms > RESEND_TIMEOUT:
+                timeSinceComms = now
+                logger.debug("Sending Status: " + ("off" if sleepActive else "on"))
+                result = tv_controller.SetPowerStatus(not sleepActive)
+                logger.debug(result)
+
+            # if (time.time() - keyPressTimeout) > TIMEOUT:
+            #     # Go To Sleep
+            #     if not sleepActive:
+            #         print "Sleep"
+            #         logger.debug("timeout: sleep")
+            #         sleepActive = True
+            #         result = tv_controller.SetPowerStatus('on' if sleepActive else 'off')
+            #         logger.debug(result)
+            #
+            #
+            #     # Comms has dropped out, let system service manager restart
+            #     # if time.time() - lastNetwork > TIMEOUT:
+            #     #     exit()
+            #
+            # else:
+            #     if sleepActive:
+            #         print "Wake"
+            #         logger.debug("timeout: wake")
+            #         sleepActive = False
+            #         result = tv_controller.SetPowerStatus(False if sleepActive else True)
+            #         logger.debug(result)
+
+            time.sleep(0.5)
 
     except Exception as e:
         logger.warn("exception in main()")
         logger.exception(e)
 
+if __name__ == "__main__":
+    main()
